d61e511763e4120a95de992f33595021301ab47f
Kjain2021 <khushboojain236@gmail.com>
2021-11-18 12:47:40 +0000

    Testing Github API call.

--- script.py
+++ script.py
@@ -1,2 +1,7 @@
-testing = "Python runs"
-print(testing)
+from github import Github 
+
+g = Github("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")
+print("testing github call")
+#Trivial details for testing.
+user=g.get_user()
+print("user"+ user.login)
