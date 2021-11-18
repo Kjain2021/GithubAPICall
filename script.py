7784e798eaadd12367e3973d989685f5f4361155
Kjain2021 <khushboojain236@gmail.com>
2021-11-18 13:48:46 +0000

    Accessing repos and filtering by topic

--- script.py
+++ script.py
@@ -1,7 +1,20 @@
 from github import Github 
 
-g = Github("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")
+
+
+
+#g = Github("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")
+g = Github("<Please add your token>")
+# please generate your personal access token 
 print("testing github call")
-#Trivial details for testing.
-user=g.get_user()
-print("user"+ user.login)
+
+#prints repos of the user
+for repo in g.get_user().get_repos():
+   print(repo.name)
+
+# Topic we are looking to use retrieve repos on 
+TOPIC = "interview-practice"
+
+# searches repos by topic
+all_repo = g.search_repositories(f'topic:{TOPIC}')
+print(all_repo.totalCount) # returns number of repositories collected with topic interview-practice  
