0219ac26eab478d41a601cf1e62ca91a38ad9315
Kjain2021 <khushboojain236@gmail.com>
2021-12-28 19:08:36 +0530

    Added another edge

--- example.py
+++ example.py
@@ -2,5 +2,7 @@ from pyvis import network as net
 from IPython.core.display import display, HTML
 g=net.Network(height='100%', width='100%',heading='testing')
 g.add_node(1)
+g.add_node(2)
+
 g.show('index.html')
 display(HTML('index.html'))
