71dcaed72e26cb51d24eb4079b381642c5fb3c8f
Kjain2021 <khushboojain236@gmail.com>
2021-12-28 19:09:28 +0530

    This one is an edge. That was another node

--- example.py
+++ example.py
@@ -3,6 +3,6 @@ from IPython.core.display import display, HTML
 g=net.Network(height='100%', width='100%',heading='testing')
 g.add_node(1)
 g.add_node(2)
-
+g.add_edge(1,2)
 g.show('index.html')
 display(HTML('index.html'))
